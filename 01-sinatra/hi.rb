4d4990d977e931e5e220d3080670be65d936393d
Alexander A~ <suvarin@yandex.ru>
2017-06-04 22:46:04 +0300

    Guessing Numbers

--- 01-sinatra/hi.rb
+++ 01-sinatra/hi.rb
@@ -2,7 +2,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 secret_number = rand(100)
+message = ''
 
 get '/' do
-  erb :index, locals: { number: secret_number }
+  guess = params[:guess].to_i
+  message = check_guess(guess, secret_number)
+  erb :index, locals: { number: secret_number, message: message }
+end
+
+def check_guess(guess, secret_number)
+  if guess == 0
+    "Welcome!"
+  elsif guess == secret_number
+    "You got it right!"
+  elsif guess > secret_number + 5
+    "Way too high!"
+  elsif guess < secret_number - 5
+    "Way too low!"
+  elsif guess > secret_number
+    "Too high!"
+  else
+    "Too low!"
+  end
 end
