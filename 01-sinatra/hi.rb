44f32d355de5729e1cb16b3b4ad7c02251b9e33b
Alexander A~ <suvarin@yandex.ru>
2017-06-04 23:40:28 +0300

    Guess limiting

--- 01-sinatra/hi.rb
+++ 01-sinatra/hi.rb
@@ -2,19 +2,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :number, rand(100)
+@@lives = 6
 @message = 'Welcome!'
 @color = 'black'
 
 get '/' do
   guess = params[:guess].to_i
   check_guess(guess)
-  erb :index, locals: { number: settings.number, message: @message, color: @color }
+  check_lives
+  erb :index, locals: { number: settings.number,
+                        message: @message,
+                        color: @color,
+                        lives: @@lives
+  }
 end
 
 def check_guess(guess)
   if guess == settings.number
     @message = "You got it right!"
     @color = 'green'
+    start_new_game
   elsif guess > settings.number + 5
     @message = "Way too high!"
     @color = '#bc0918'
@@ -28,4 +35,17 @@ def check_guess(guess)
     @message = "Too low!"
     @color = '#f79c1d'
   end
+end
+
+def check_lives
+  @@lives -= 1
+  if @@lives == 0
+    @message = "You lost! A new number has been generated. The number was #{settings.number}"
+    start_new_game
+  end
+end
+
+def start_new_game
+  settings.number = rand(100)
+  @@lives = 6
 end
