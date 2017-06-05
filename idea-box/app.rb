f00bf879085a7af4c408acf665fbb2f8ead94f9c
Alexander A~ <suvarin@yandex.ru>
2017-06-05 18:54:24 +0300

    I1: Recording Ideas

--- idea-box/app.rb
+++ idea-box/app.rb
@@ -1,6 +1,16 @@
+require './idea'
 class IdeaBoxApp < Sinatra::Base
   get '/' do
-    'Hello, World!'
+    erb :index
   end
 
+  post '/' do
+    idea = Idea.new
+    idea.save
+    'CREATING an idea!'
+  end
+
+  not_found do
+    erb :error
+  end
 end
