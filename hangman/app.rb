7ebed6aa8891d006f0694a9334cfc4d31f490cba
Alexander A~ <suvarin@yandex.ru>
2017-06-08 19:58:16 +0300

    Add New Game Session

--- hangman/app.rb
+++ hangman/app.rb
@@ -1,9 +1,29 @@
+require './models/hangman'
 set :slim, pretty: true
+set :session_secret, "something"
+
+enable :sessions
 
 get '/' do
   slim :index
 end
 
+post '/new' do
+  h = Hangman.new
+  session[:secret]      = h.secret_word
+  session[:lives]       = 10
+  session[:incorrect]   = h.incorrect_letters
+  session[:placeholder] = h.placeholder
+  session[:available]   = h.available_letters
+  redirect '/game'
+end
+
 get '/game' do
-  slim :game
+  slim :game, locals: {
+    secret: session[:secret],
+    lives: session[:lives],
+    incorrect: session[:incorrect],
+    placeholder: session[:placeholder],
+    available: session[:available]
+  }
 end
